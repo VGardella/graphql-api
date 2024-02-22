5a64ce44fd60a75a3e4f162112848bed88aec3c5
Victoria Gardella <vgardella@teknos.local>
2024-02-22 16:31:02 -0300

    add server and database conection

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const schema = require('./schemas/index.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost:27017/local')
+    .then(() => {
+        console.log('Conexion exitosa a la base de datos');
+    })
+    .catch((err) => {
+        console.log('Error en la base de datos:', err);
+    })
+
+app.use(express.json());
+app.use('/api',
+    graphqlHTTP((req) => {
+        return {
+            schema: schema,
+            graphiql: true,
+            context: {
+                user: req.user,
+                req: req
+            }
+        }
+    })
+)
+
+app.listen(PORT, () => {
+    console.log('Escuchando puerto:', PORT);
+})
